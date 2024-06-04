db3ebba82e5d8b78aa931fc4fcedca34e0cd7d9d
ɪᴛᴀᴄʜɪ ᴜᴄʜɪʜᴀ <164128865+Itachiguchiha@users.noreply.github.com>
2024-06-04 18:29:57 +0530

    Update __init__.py

--- NekoRobot/__init__.py
+++ NekoRobot/__init__.py
@@ -342,7 +342,5 @@ TIGERS = list(TIGERS)
 # Load at end to ensure all prev variables have been set
 
 # make sure the regex handler can take extra kwargs
-tg.CommandHandler = CustomCommandHandler
-tg.MessageHandler = CustomMessageHandler
 
 LOGGER.info("[NEKOROBOT IS READY]")
